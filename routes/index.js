f0fd47dc3e603f24433b90fdb5dcf305918c180d
Myron Young <myronyoung@Myrons-MacBook-Pro.local>
2018-11-15 16:02:23 -0500

    Added user routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,32 @@
 const express = require('express')
 const router = express.Router()
+const usersController = require('../controllers/usersController')
+const eventsController = require('../controllers/eventsController')
+const itemsController = require('../controllers/itemsController')
 
+//routes for reading
+router.get('/api/users', usersController.index)
+router.get('/api/users/:userId', usersController.show)
+
+
+
+
+// //create routes
+router.post('/api/users/', usersController.create)
+// router.post('/users', usersController.create)
+// router.post('/users', userController.create )
+
+
+
+// //delete routes 
+router.delete('/api/users/:userId', usersController.delete)
+// router.delete('/users/:usersId', usersController.delete)
+// router.delete('/users/:usersId/houses/:housesId', housesController.delete)
+
+// //update routes 
+router.patch('/api/users/:userId', usersController.update)
+// router.patch('/users/:usersId', usersController.update)
+// router.patch('/users/:usersId/houses/:housesId', housesController.update)
 
 
 
