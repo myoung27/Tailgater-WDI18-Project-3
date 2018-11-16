c6137d3e813924db0cadbda495393d095aa250ca
Myron Young <myronyoung@Myrons-MacBook-Pro.local>
2018-11-16 16:42:08 -0500

    Added React routes

---
+++ client/src/components/GamePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class GamePage extends Component {
+  render() {
+    return (
+      <div>
+        Hello from the Game Page
+      </div>
+    )
+  }
+}
---
+++ client/src/components/ItemsPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ItemsPage extends Component {
+  render() {
+    return (
+      <div>
+        Hello From the Items Page
+      </div>
+    )
+  }
+}
